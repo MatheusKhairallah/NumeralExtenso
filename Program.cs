dd83f4ab47db15642ae4b09bfe7cf95801640bec
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 22:01:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Humanizer;
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva qualquer número e te darei ele em extenso.");
+            Console.Write("Digíte o número: ");
+            int numero=Convert.ToInt32(Console.ReadLine());
+            string numero2=numero.ToWords();
+            Console.Clear();
+            Console.WriteLine($"Seu número por extenso é= {numero2}");
+        }
+    }
+}
